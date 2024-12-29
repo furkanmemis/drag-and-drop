24fe652fc14a19c327ecbca5b7a3e2e00890f7b1
furkanmemis <fmemis0160@gmail.com>
2024-12-29 13:18:28 +0300

    updated layers component

--- src/components/Layers.tsx
+++ src/components/Layers.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Typography, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-interface LayerProps{
-    items: string[];
+interface LayerProps {
+  items: string[];
 }
 
-const Layers: React.FC<LayerProps> = ({items}) => {
+const Layers: React.FC<LayerProps> = ({ items }) => {
   const [allComponent, setAllComponent] = useState<string[]>([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setAllComponent(items);
-  },[items]);
+  }, [items]);
+
+  const deleteItem = (removedIndex: number) => {
+    const allItem = allComponent.filter((_, index) => index !== removedIndex);
+    setAllComponent(allItem);
+  };
 
   return (
     <Box
@@ -21,20 +26,40 @@ const Layers: React.FC<LayerProps> = ({items}) => {
         backgroundColor: "white",
         minWidth: "20vw",
         position: "absolute",
-        bottom: 0,
+        bottom: 50,
         borderTop: "2px solid rgba(0, 0, 0, 0.3)",
         borderBottom: "2px solid rgba(0, 0, 0, 0.3)",
-        width: "100%"
+        width: "100%",
+        maxHeight: "30vh",
+        overflow: "auto",
       }}
     >
-      <Typography style={{ display: "flex", justifyContent: "center", fontWeight: "bold", borderBottom: "2px solid rgba(0, 0, 0, 0.3)" }} variant="h6">
+      <Typography
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          fontWeight: "bold",
+          borderBottom: "2px solid rgba(0, 0, 0, 0.3)",
+        }}
+        variant="h6"
+      >
         Layers
       </Typography>
 
       {allComponent.map((component, index) => (
-        <Box key={index} sx={{ margin: 1, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Box
+          key={index}
+          sx={{
+            margin: 1,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Typography>{component}</Typography>
-          <DeleteIcon style={{ color: "#B03A2E" }} />
+          <IconButton onClick={()=>{deleteItem(index)}}>
+            <DeleteIcon style={{ color: "#B03A2E" }} />
+          </IconButton>
         </Box>
       ))}
     </Box>
